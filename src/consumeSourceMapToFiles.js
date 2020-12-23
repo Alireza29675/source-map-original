2a91f7fc32a612b6eb8bbf2376a4424e82a46297
Pouya <pouya@cafebazaar.ir>
2020-12-23 21:55:00 +0330

    Fix some source codes coming out of the output

--- src/consumeSourceMapToFiles.js
+++ src/consumeSourceMapToFiles.js
@@ -4,13 +4,15 @@ const mkdirp = require('mkdirp');
 const { SourceMapConsumer } = require('source-map');
 const urlPathWithoutFile = require('./utils/urlPathWithoutFile');
 
+
 module.exports = async function consumeSourceMapToFiles({ sourceMapContents, outputPath }){
     for(const { url, content } of sourceMapContents){
         const basePath = urlPathWithoutFile(url);
         
         await SourceMapConsumer.with(content, null, (consumer) => {
             for(const source of consumer.sources){
-                const pathToSource = path.join(outputPath, basePath, source);
+                const partialPathToSource = path.join(basePath, source);
+                const pathToSource = path.join(outputPath, partialPathToSource);
                 mkdirp.sync(path.dirname(pathToSource));
                 fs.writeFileSync(pathToSource, consumer.sourceContentFor(source));
             }
